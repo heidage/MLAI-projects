dcb2423bb1934761a3db4fd56fa08880be567e0f
Shreyas Hegde <75136935+Shreyas-hacker@users.noreply.github.com>
2023-05-11 01:59:58 +0800

    1 done project

---
+++ Computer Vision/Digit Recognition/backend/server.py
@@ -0,0 +1,38 @@
+from flask import Flask, request
+from flask_cors import CORS
+import tensorflow as tf
+import numpy as np
+import base64
+import cv2
+import json
+
+model = tf.keras.models.load_model('../model/final_model.h5')
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/process_image',methods=['POST'])
+def predict_digit():
+    image = request.json.get('image')
+    if image != "":
+        image_array = url_to_image(image)
+        result = model.predict(image_array)
+        prediction = np.argmax(result,axis=1)[0]
+        return {'image_prediction': int(prediction)}
+    else:
+        return {'image_prediction': "Nope"}
+
+def url_to_image(url):
+    image_64 = url.split(',')[1]
+    binary = base64.b64decode(image_64)
+    image = np.asarray(bytearray(binary),dtype='uint8')
+    image = cv2.imdecode(image,cv2.IMREAD_GRAYSCALE)
+    pred_img_resize = cv2.resize(image,(28,28))
+    pred_img_resize = cv2.bitwise_not(pred_img_resize)
+
+    pred_img_normalized = pred_img_resize.astype('float32')/255.0
+    pred_img_normalized = pred_img_normalized.reshape(-1,28,28,1)
+
+    return pred_img_normalized
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ Computer Vision/Digit Recognition/frontend/src/App.js
@@ -0,0 +1,223 @@
+import {useState, useEffect,useRef} from 'react';
+import axios from 'axios';
+import './App.css';
+import 'fontsource-roboto';
+import Typography from '@mui/material/Typography'
+import AppBar from '@mui/material/AppBar'
+import ToolBar from '@mui/material/Toolbar'
+import Button from '@mui/material/Button'
+import IconButton from '@mui/material/IconButton'
+import Grid from '@mui/material/Grid'
+import CssBaseline from "@mui/material/CssBaseline";
+import {ThemeProvider, createTheme} from '@mui/material/styles';
+import { makeStyles } from '@mui/styles';
+import { blue, red } from '@mui/material/colors';
+import Brightness6Icon from '@mui/icons-material/Brightness6';
+
+const useStyles = makeStyles({
+  root:{
+    flexGrow: 1,
+  },
+  title:{
+    textAlign: 'center',
+  },
+  subtitle:{
+    textAlign: 'center',
+  },
+  buttonContainer:{
+    marginBottom: '20px',
+  },
+  navHeader:{
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  heading:{
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 5,
+  },
+  toggle:{
+    marginTop: 10,
+  },
+  body:{
+    width: '60%',
+    marginBottom: 20,
+  }
+})
+
+const DarkTheme = createTheme({
+  palette: {
+    primary: {
+      main: red[500]
+    },
+    secondary: {
+      main: blue[400]
+    },
+    background: {
+      default: '#282c34',
+    },
+    text: {
+      primary: '#fff',
+    },
+  }
+})
+
+const LightTheme = createTheme({
+  palette: {
+    primary: {
+      main: blue[500]
+    },
+    secondary: {
+      main: red[400]
+    },
+    background: {
+      default: '#fff',
+    },
+    text: {
+      primary: '#282c34',
+    },
+  }
+})
+
+function App() {
+  const classes = useStyles();
+  const canvasRef = useRef(null);
+  const[imageData, setImageData] = useState("");
+  const[response, setResponse] = useState("Loading...");
+  const[theme, setTheme] = useState(true);
+  const [isDrawing, setIsDrawing] = useState(false);
+  const [lastX, setLastX] = useState(0);
+  const [lastY, setLastY] = useState(0);
+  const appliedTheme = theme ? LightTheme : DarkTheme;
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+    ctx.lineJoin = 'round';
+    ctx.lineCap = 'round';
+    ctx.lineWidth = 10;
+  }, []);
+
+  const clearCanvas = () => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  };
+
+  const recognizeDigit = () => {
+    const canvas = canvasRef.current;
+    const imageBase64 = canvas.toDataURL()
+
+    // Send the image data to the server
+    axios
+      .post(`http://127.0.0.1:5000/process_image`,{
+          image:imageBase64,
+      })
+      .then((res) => {
+        setResponse(res.data.image_prediction);
+      });
+  };
+
+  const handleMouseDown = (e) => {
+    setIsDrawing(true);
+    setLastX(e.nativeEvent.offsetX);
+    setLastY(e.nativeEvent.offsetY);
+  };
+
+  const handleMouseMove = (e) => {
+    if (!isDrawing) return;
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+    const x = e.nativeEvent.offsetX;
+    const y = e.nativeEvent.offsetY;
+    ctx.beginPath();
+    ctx.moveTo(lastX, lastY);
+    ctx.lineTo(x, y);
+    ctx.stroke();
+    setLastX(x);
+    setLastY(y);
+  };
+
+  const handleMouseUp = () => {
+    setIsDrawing(false);
+  };
+
+
+  return (
+    <ThemeProvider theme={appliedTheme}>
+      <CssBaseline />
+      <div className="container">
+        <div className={classes.root}>
+          <AppBar>
+            <ToolBar>
+              <Grid justify={"space-between"} container>
+                <Grid xs={1} item></Grid>
+                <Grid xs={4} item>
+                  <Grid container justify={"center"}>
+                    <div className={classes.heading}>
+                      <Typography variant="h4">
+                        AI Digits
+                      </Typography>
+                      <Typography variant="subtitle1">
+                        A rudimentary take on Neural Networks
+                      </Typography>
+                    </div>
+                  </Grid>
+                </Grid>
+                <Grid xs={1} item>
+                  <IconButton
+                    onClick={() => setTheme(!theme)}
+                    className={classes.toggle}
+                  >
+                    <Brightness6Icon />
+                  </IconButton>
+                </Grid>
+              </Grid>
+              </ToolBar>
+          </AppBar>
+        </div>    
+        <Typography variant="body1" className={classes.body} align="justify">
+          As a side project in artificial intelligence, I created a 2 layer Convolutional Neural Network using tensorflow in Python. I decided to turn it into a web application with React and Flask. It's not the greatest model, but it's a decent attempt. As a 28x28 input image, the net has 784 input nodes, 2 Convolutional layers, 1 Dense Layer with 128 neurons and 10 output nodes. The canvas below is a bit less accurate since it's a 280x280 canvas scaled down and redrawn with the back-end. With the MNIST database, the neural network hits about 97-98% accuracy.
+        </Typography>
+        <Typography>
+          Draw a digit from 0-9
+        </Typography>
+        <div className="canvas">
+          <canvas
+            ref={canvasRef}
+            width={280}
+            height={280}
+            onMouseDown={handleMouseDown}
+            onMouseMove={handleMouseMove}
+            onMouseUp={handleMouseUp}
+          />
+        </div>
+        <div className={classes.buttonContainer}>
+          <Button 
+            onClick={clearCanvas}
+            color="secondary"
+            variant="outlined"
+          >
+            Reset
+          </Button>
+          <Button 
+            onClick={recognizeDigit}
+            color="primary"
+            variant="outlined"
+          >
+            Guess
+          </Button>
+        </div>
+        <Typography variant="h5">
+          Your number is {response}
+        </Typography>
+      </div>
+    </ThemeProvider>
+  );
+}
+
+export default App;
