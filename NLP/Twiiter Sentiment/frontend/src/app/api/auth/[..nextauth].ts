819f5f45f99cc988f2c24991cd9774b74f4445b7
Shreyas Hegde <75136935+Shreyas-hacker@users.noreply.github.com>
2023-12-23 00:54:06 +0800

    added NextAuth js

---
+++ NLP/Twiiter Sentiment/frontend/src/app/api/auth/[..nextauth].ts
@@ -0,0 +1,21 @@
+import NextAuth, {NextAuthOptions} from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+import FacebookProvider from 'next-auth/providers/facebook';
+
+export const authOptions: NextAuthOptions = {
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_ID as string,
+            clientSecret: process.env.GOOGLE_SECRET as string,
+            authorization:{
+                params: {
+                    prompt: "consent",
+                    access_type: "offline",
+                    response_type: "code"
+                }
+            }
+        })
+    ]
+}
+
+export default NextAuth(authOptions);
--- NLP/Twiiter Sentiment/frontend/src/app/page.tsx
+++ NLP/Twiiter Sentiment/frontend/src/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image'
 import styles from './page.module.css'
 import Dashboard from './dashboard/page'
 import Header from '@/components/Header'
