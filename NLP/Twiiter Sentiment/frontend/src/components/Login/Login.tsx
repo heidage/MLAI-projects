ba621ca40a126779eac74fbd6472609459d33d7f
Shreyas Hegde <75136935+Shreyas-hacker@users.noreply.github.com>
2024-01-02 22:50:15 +0800

    added login details

--- NLP/Twiiter Sentiment/frontend/src/components/Login/Login.tsx
+++ NLP/Twiiter Sentiment/frontend/src/components/Login/Login.tsx
@@ -6,12 +6,13 @@ import { Avatar } from "@mui/material";
 
 const Login = ()=>{
     const {data: session } = useSession();
-
+    const avatar = session?.user?.image as string;
+    
     if (session){
         return <>
             Signed in as {session?.user?.email} <br/>
             <p>Welcome {session?.user?.name}</p>
-            <Avatar alt={session?.user?.name} src={session?.user?.image}/>
+            <Avatar alt={session?.user?.name as string} src={avatar}/>
             <button onClick={()=>signOut()}>Sign Out</button>
         </>
     }
