36a2a6749ed81bb96b58db359100ce3238f2861d
Shreyas Hegde <75136935+Shreyas-hacker@users.noreply.github.com>
2024-01-06 01:27:26 +0800

    added login functionality and header component

--- NLP/Twiiter Sentiment/frontend/src/components/Login/Login.tsx
+++ NLP/Twiiter Sentiment/frontend/src/components/Login/Login.tsx
@@ -3,22 +3,20 @@
 import React from "react";
 import { useSession, signIn, signOut } from "next-auth/react";
 import { Avatar } from "@mui/material";
+import Button from "@mui/material/Button";
 
 const Login = ()=>{
     const {data: session } = useSession();
     const avatar = session?.user?.image as string;
-    
+
     if (session){
         return <>
-            Signed in as {session?.user?.email} <br/>
-            <p>Welcome {session?.user?.name}</p>
-            <Avatar alt={session?.user?.name as string} src={avatar}/>
-            <button onClick={()=>signOut()}>Sign Out</button>
+            <Button variant={'contained'} color={'error'} onClick={()=>signOut()}>Sign Out</Button>
         </>
     }
     return <>
-        Not signed in <br/>
-        <button onClick={()=>signIn()}>Sign In</button>
+        <h2>Please log in</h2> <br/>
+        <Button variant={'contained'} color={'success'} onClick={()=>signIn()}>Sign In</Button>
     </>
 }
 
